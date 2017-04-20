f5842a1b5966e7fef8763df8279afb284968e660
opensofias <sofias@radikalismus.com>
2017-04-20 08:25:43 +0200

    used repeatedReplace throughout seedGen

--- seed.js
+++ seed.js
@@ -1,17 +1,7 @@
 'use strict'
 
-var thueMorse = iter => recurTimes (morseStep, iter, "0")
-
-var rabbit = iter => recurTimes (rabbitStep, iter, "0")
-
 var repeatedReplace = (iterations, first, ...lookup) =>
 	recurTimes (repStep, Number.parseInt(iterations), first, lookup)
-
-var rabbitStep = prev =>
-	prev.split ("").map (s => s == 1 ? "10" : "1").join ("")
-
-var morseStep = prev =>
-	prev.split ("").map (s => s == 1 ? "10" : "01").join ("")
 
 var repStep = (prev, array) =>
 	prev.split ("").map (s => array[Number.parseInt(s)] || "").join ("")
@@ -21,15 +11,16 @@ var recurTimes = (fun, times, step, ...params) =>
 	while (times -- > 0) step = fun(step, ...params)
 	return step
 }
+
 var seedGen = seedString =>
 {
 	const seedParam = seedString.slice (2)
 	switch (seedString.slice (0, 2))
 	{
-		case ("tm"):
-			return thueMorse (Number.parseInt(seedParam))
-		case ("rb"):
-			return rabbit (Number.parseInt(seedParam))
+		case ("tm"): // Thue-Morse sequence
+			return repeatedReplace (seedParam, "0", "01", "10")
+		case ("rb"): // Fibonacci rabbit sequence
+			return repeatedReplace (seedParam, "0", "1", "10")
 		case ("rr"):
 			return repeatedReplace (...seedParam.split("."))
 	}
