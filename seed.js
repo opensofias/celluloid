0f1020f271d69ac65cfb96bf03d6ba364efffe7c
opensofias <sofias@radikalismus.com>
2017-04-27 19:27:48 +0200

    replaced switch with object in seed.js

--- seed.js
+++ seed.js
@@ -11,17 +11,16 @@ var recurTimes = (fun, times, step, ...params) => {
 	return step
 }
 
-var seedGen = seedString => {
-	const seedParam = seedString.slice (2)
-	switch (seedString.slice (0, 2)) {
-		case ('tm'): // Thue-Morse sequence
-			return repeatedReplace (seedParam, '0', '01', '10')
-		case ('rb'): // Fibonacci rabbit sequence
-			return repeatedReplace (seedParam, '0', '1', '10')
-		case ('cr'): // single core
-			return repeatedReplace (seedParam, '010', '00', '1')
-		case ('ts'): // transition
-			return repeatedReplace (seedParam, '01', '00', '11')
-		case ('rr'):
-			return repeatedReplace (...seedParam.split('.'))
-}}
+const seedLib = {
+	tm: ['0', '01', '10'],
+	rb: ['0', '1', '10'],
+	cr: ['010', '00', '1'],
+	ts: ['01', '00', '11'],
+}
+
+var seedGen = string => {
+	const [kind, param]
+	= [seedLib[string.slice (0, 2)], string.slice (2)]
+	if (kind) return repeatedReplace (param, ...kind)
+	else return repeatedReplace (...param.split('.'))
+}
