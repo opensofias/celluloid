29deecf2284ec24f9437e77c0c822f494ec57c0c
opensofias <12581975+opensofias@users.noreply.github.com>
2021-12-13 18:00:43 +0100

    superDragon as generalized dragon curve funcion

--- seed.js
+++ seed.js
@@ -16,19 +16,19 @@ const generateSeed = seedCode => {
 	else throw "invalid seed code: " + seedCode
 }
 
+const superDragon = (iterations, fillers = ['1', '0'], strings = []) => 
+	iterations <= -1 ? strings :
+		superDragon (iterations - 1, fillers,
+			fillers.map (filler => strings.join(filler))
+		)
+
 // todo: more parameterized seeds in general would be nice
 const seedFunctions = {
 	tm: repeatedSubstitution ('0', '01', '10'), // Thue-Morse sequence
 	rb: repeatedSubstitution ('0', '1', '10'), // rabbit sequence
 	cr: repeatedSubstitution ('010', '00', '1'), // core (single 1 surrounded by 0s)
 	ts: repeatedSubstitution ('01', '00', '11'), // transition (0 on one side, ones at the other)
-	dc (iterations) { // dragon curve sequence
-		let strings = ['', '']
-		while (iterations -- > 0) {
-			strings = [strings.join ('1'), strings.join ('0')]
-		}
-		return strings [0]
-	},
+	dc (iterations) { return superDragon (iterations) [0] },
 	kk (iterations) { // kolakosky series mod 2
 		const seed = [1,2]
 		const length = seed.length
