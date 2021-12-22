a14aab14ffad9381ae3f476e0239243043fba861
opensofias <12581975+opensofias@users.noreply.github.com>
2021-12-22 01:15:10 +0100

    recursiveSubstitution -> recurSubst

--- seed.js
+++ seed.js
@@ -10,18 +10,18 @@ const superDragon = (fillers = ['1', '0'], strings = []) => iter =>
 		fillers.map (filler => strings.join(filler))
 	) (iter - 1)
 
-const recursiveSubstitution = (lookup, prev = '0') => iter =>
-	iter <= 0 ? prev : recursiveSubstitution (
+const recurSubst = (lookup, prev = '0') => iter =>
+	iter <= 0 ? prev : recurSubst (
 		lookup,
 		prev.split ('').map (idx => lookup[Number.parseInt(idx)] || '').join ('')
 	) (iter - 1)
 
 // todo: more parameterized seeds in general would be nice
 const seedFunctions = {
-	tm: recursiveSubstitution (['01', '10']), // Thue-Morse sequence
-	rb: recursiveSubstitution (['1', '10']), // rabbit sequence (aka fibonacci word)
-	cr: recursiveSubstitution (['00', '1'], '010'), // core (single 1 surrounded by 0s)
-	ts: recursiveSubstitution (['00', '11'], '01'), // transition (0 on one side, ones at the other)
+	tm: recurSubst (['01', '10']), // Thue-Morse sequence
+	rb: recurSubst (['1', '10']), // rabbit sequence (aka fibonacci word)
+	cr: recurSubst (['00', '1'], '010'), // core (single 1 surrounded by 0s)
+	ts: recurSubst (['00', '11'], '01'), // transition (0 on one side, ones at the other)
 	dc: superDragon (['1', '0']),
 	kk: iter => { // kolakosky series mod 2
 		const seed = [1,2]
