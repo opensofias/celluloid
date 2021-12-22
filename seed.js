74d1d95a876f774344068951a3cc657cad22c332
opensofias <12581975+opensofias@users.noreply.github.com>
2021-12-22 22:36:30 +0100

    added radomDigits seed

--- seed.js
+++ seed.js
@@ -27,6 +27,12 @@ const recurSubst = (lookup, prev = '0') => iter =>
 		prev.split ('').map (idx => lookup[Number.parseInt(idx)] || '').join ('')
 	) (iter - 1)
 
+const randomDigits = (threshold = .5) => lengthLog2 =>
+	new Array (Math.ceil (2 ** Number.parseFloat (lengthLog2)))
+	.fill (0)
+	.map (() => Math.random () > threshold ? 1 : 0)
+	.join ()
+
 	// todo: more parameterized seeds in general would be nice
 const seedFunctions = {
 	tm: recurSubst (['01', '10']), // Thue-Morse sequence
@@ -34,5 +40,6 @@ const seedFunctions = {
 	cr: recurSubst (['00', '1'], '010'), // core (single 1 surrounded by 0s)
 	ts: recurSubst (['00', '11'], '01'), // transition (0 on one side, ones at the other)
 	dc: superDragon (['1', '0']),
-	kk: iter => superKolakosky ([1, 2]) (iter).map (x => x -1).join ('')
+	kk: iter => superKolakosky ([1, 2]) (iter).map (x => x -1).join (''),
+	rd: randomDigits (.5)
 }
