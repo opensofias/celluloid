ff03a7f7eb1d47eed28c7d56618b734755b0f56d
opensofias <12581975+opensofias@users.noreply.github.com>
2021-12-26 12:29:01 +0100

    added "only" parameter1

--- main.js
+++ main.js
@@ -26,9 +26,13 @@ const setDefaults = (config = {}) => ({
 const makeAll = config => {
 	removeAll(['canvas', 'svg'])
 	updateNav (config)
-	let {neighbors, radix, start, end, page, amount, seed} = config
-	start = start ?? (page ? page * amount : 0)
-	end = end ?? ((amount != -1) ? start + amount : radix ** (radix ** neighbors))
+	let {neighbors, radix, start, end, page, amount, seed, only} = config
+	start = start ?? only ??
+		(page ? page * amount : 0)
+	end = end ?? (only ? only + 1 :
+		(amount != -1) ? start + amount :
+		radix ** (radix ** neighbors)
+	)
 	let ruleNum = start;
 	const rolloutFun = rollout (config) (generateSeed (seed))
 	do {
